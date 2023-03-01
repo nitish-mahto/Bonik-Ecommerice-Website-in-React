c59757df27dc46fb7e60ed147b0a83b5477e8bfa
Nitish Mahto <mahtonitishkumar8865@gmail.com>
2023-03-01 15:23:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react"
+import "./App.css"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Header from "./common/header/Header"
+import Pages from "./pages/Pages"
+import Data from "./components/Data"
+import Cart from "./common/Cart/Cart"
+import Footer from "./common/footer/Footer"
+import Sdata from "./components/shops/Sdata"
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Header CartItem={CartItem} />
+        <Switch>
+          <Route path='/' exact>
+            <Pages productItems={productItems} addToCart={addToCart} shopItems={shopItems} />
+          </Route>
+          <Route path='/cart' exact>
+            <Cart CartItem={CartItem} addToCart={addToCart} decreaseQty={decreaseQty} />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </>
+  )
+}
+
+export default App
